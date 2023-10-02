53ab49e8fddb6dc462837c93b77ad790ccf5ec6d
MariaFdaChavarria <mchavarria10233@ufide.ac.cr>
2023-10-01 19:24:22 -0600

    Actualizacion de olvidar contraseña y crud usuario

--- SMRA2023/Controllers/UsuarioController.cs
+++ SMRA2023/Controllers/UsuarioController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using SMRA2023.Entities;
 using SMRA2023.Interfaces;
 
@@ -23,16 +24,54 @@ namespace SMRA2023.Controllers
         public IActionResult validarusuario(UsuarioEntities usuario)
         {
 
-            var resultado = _usuario.validarUsuario(usuario);
-            if(resultado != null)
+            var result = _usuario.validarUsuario(usuario);
+            if(result != null)
             {
+                long userID = result.IdUser;
+
+                HttpContext.Session.SetInt32("IdSession",(int)userID);
+                HttpContext.Session.SetString("NameSession",result.userName);
+                string NameSession = HttpContext.Session.GetString("NameSession");
+                ViewBag.NameSession = NameSession;
+
                 return RedirectToAction("Index","Home");
+
             }else 
             {
                 TempData["MensajeError"] = "Usuario incorrecto o no existe.";
                 return RedirectToAction("Index", "Usuario");
             }
         }
+
+
+
+        [HttpGet]
+        public IActionResult RecoverPW()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult email_Recovery(UsuarioEntities usuario)
+        {
+            var resultado = _usuario.email_Verification(usuario);
+            if (resultado != null)
+            {
+                _usuario.Email(usuario.email);
+                TempData["ContrasenaRecuperada"] = "Su contraseña se envió al correo ingresado.";
+                return RedirectToAction("RecoverPW", "Usuario");
+                // Su contraseña se envió al correo ingresado.
+            }
+            else
+            {
+                TempData["ErrorCorreo"] = "No tienes una cuenta, por favor registrate.";
+                // No tienes una cuenta, por favor registrate.
+                return RedirectToAction("RecoverPW", "Usuario");
+
+            }
+
+        }
+
 
         [HttpGet]
         public IActionResult Register()
@@ -58,19 +97,27 @@ namespace SMRA2023.Controllers
             }
         }
 
+          
         [HttpGet]
-        
         public IActionResult Profile()
         {
-            return View();
+           var IdUser = HttpContext.Session.GetInt32("IdSession");
+
+            long UserId = (long)IdUser;
+
+            var result = _usuario.ConsultAcc(UserId);
+
+            if (result != null)
+            {
+                return View(result);
+            }
+
+            return NotFound(); // Maneja el caso en el que el usuario no se encuentre
         }
 
         [HttpPost]
         public IActionResult UpdateUser(UsuarioEntities usuario)
         {
-            usuario.idRol = 2;
-            usuario.statusU = true;
-
             var resultado = _usuario.UpdateUser(usuario);
 
             if (resultado != null)
@@ -79,13 +126,37 @@ namespace SMRA2023.Controllers
             }
             else
             {
-                return RedirectToAction("Index", "Home");
+                return RedirectToAction("Index", "Usuario");
             }
         }
 
+        [HttpPost]
+        public IActionResult RegisterProgram()
+        {
+            var IdUser = HttpContext.Session.GetInt32("IdSession");
+
+            long UserId = (long)IdUser;
+
+            var result = _usuario.RegUserProg(UserId);
+
+            if (result != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Usuario");
+            }
+        }
+
+
     }
 
+
+
 }
+
+
 
  
 
--- SMRA2023/Interfaces/IUsuarioModel.cs
+++ SMRA2023/Interfaces/IUsuarioModel.cs
@@ -6,9 +6,17 @@ namespace SMRA2023.Interfaces
     {
         public UsuarioEntities? validarUsuario(UsuarioEntities usuario);
 
+        public UsuarioEntities? email_Verification(UsuarioEntities usuario);
+        public void Email(string correo);
         public UsuarioEntities? RegisterUser(UsuarioEntities usuario);
 
+        public UsuarioEntities? ConsultAcc(long qIdUser);
+
         public UsuarioEntities? UpdateUser(UsuarioEntities usuario);
+
+        public UsuarioEntities? RegUserProg(long IdUser);
+
+
     }
 
 
